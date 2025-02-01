c24babb2e30caa8ccc107ac71687964b6ffae996
elay.glazer <elay.glazer@controlup.com>
2025-02-01 15:22:36 +0200

    added red box

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       arjs="sourceType: webcam;"
       renderer="antialias: true; colorManagement: true; highRefreshRate: true;"
     >
+      <a-box 
+        position="0 1 -3" 
+        rotation="0 45 0" 
+        color="red" 
+        depth="0.5" 
+        height="0.5" 
+        width="0.5"
+      ></a-box>
+
       <a-entity camera></a-entity>
     </a-scene>
   );
