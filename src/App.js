5e733d34467da048f399e4770355acd4439f2304
elay.glazer <elay.glazer@controlup.com>
2025-02-01 14:53:11 +0200

    fixing camera

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <a-scene embedded arjs="sourceType: webcam;">
-      <a-entity camera></a-entity>
-    </a-scene>
+      <a-entity camera position="0 1.6 0" wasd-controls-enabled="false" look-controls-enabled="false" fov="60"></a-entity>
+      </a-scene>
   );
 }
 
