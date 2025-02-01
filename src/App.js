23d989521fda54cf44eadc2438aa5584d5388b48
elay.glazer <elay.glazer@controlup.com>
2025-02-01 15:37:11 +0200

    added red box

--- src/App.js
+++ src/App.js
@@ -8,16 +8,13 @@ function App() {
       renderer="antialias: true; colorManagement: true; highRefreshRate: true;"
       xr-mode-ui="enabled: false"
     >
-      <!-- Lighting for better visibility -->
       <a-entity 
         light="type: directional; intensity: 1.0; castShadow: true; color: white" 
         position="5 10 7"
       ></a-entity>
 
-      <!-- Camera -->
       <a-camera position="0 2 2"></a-camera>
 
-      <!-- Red Cube with Rotation -->
       <a-box 
         xrextras-one-finger-rotate
         position="0 0.5 -2"
@@ -25,7 +22,6 @@ function App() {
         shadow
       ></a-box>
 
-      <!-- Ground Plane for Reference -->
       <a-plane 
         height="2000" 
         width="2000" 
