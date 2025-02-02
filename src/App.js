8d08ad3a1f2990647d50ee2d81742843194c3da0
elay.glazer <elay.glazer@controlup.com>
2025-02-02 21:37:10 +0200

    ar from start

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <a-scene 
-      embedded 
-      arjs="sourceType: webcam;"
-      renderer="antialias: true; colorManagement: true; highRefreshRate: true;"
-      xr-mode-ui="enabled: false"
-    > 
-      <a-entity light="type:directional; castShadow:true; color:white; intensity:1.0" position="5 10 7"></a-entity>
-      <a-camera position="0 2 2"></a-camera>
-      <a-box xrextras-one-finger-rotate position="0 0.5 0" material="color: #AD50FF;" shadow></a-box>
-      <a-plane height="2000" width="2000" rotation="-90 0 0" material="shader: shadow; opacity: 0.67" shadow></a-plane> 
+    <a-scene embedded arjs="sourceType: webcam;">
+      <a-camera></a-camera>
     </a-scene>
   );
 }
