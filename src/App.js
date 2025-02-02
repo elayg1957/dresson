08e9971cff7f5e4c080fbda7272c543c7c1fd6fc
elay.glazer <elay.glazer@controlup.com>
2025-02-02 07:30:53 +0200

    changed to pink cube

--- src/App.js
+++ src/App.js
@@ -7,29 +7,11 @@ function App() {
       arjs="sourceType: webcam;"
       renderer="antialias: true; colorManagement: true; highRefreshRate: true;"
       xr-mode-ui="enabled: false"
-    >
-      <a-entity 
-        light="type: directional; intensity: 1.0; castShadow: true; color: white" 
-        position="5 10 7"
-      ></a-entity>
-
+    > 
+      <a-entity light="type:directional; castShadow:true; color:white; intensity:1.0" position="5 10 7"></a-entity>
       <a-camera position="0 2 2"></a-camera>
-
-      <a-box 
-        xrextras-one-finger-rotate
-        position="0 0.5 -2"
-        material="color: red"
-        shadow
-      ></a-box>
-
-      <a-plane 
-        height="2000" 
-        width="2000" 
-        rotation="-90 0 0" 
-        material="shader: shadow; opacity: 0.67"
-        shadow
-      ></a-plane>
-      
+      <a-box xrextras-one-finger-rotate position="0 0.5 0" material="color: #AD50FF;" shadow></a-box>
+      <a-plane height="2000" width="2000" rotation="-90 0 0" material="shader: shadow; opacity: 0.67" shadow></a-plane> 
     </a-scene>
   );
 }
