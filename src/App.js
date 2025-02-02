1ad8ab4666b5c5a526ba0c2b7111709efdba3e2e
elay.glazer <elay.glazer@controlup.com>
2025-02-02 21:42:31 +0200

    added red cube

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import React from "react";
 function App() {
   return (
     <a-scene embedded arjs="sourceType: webcam;">
+      <!-- Floating Cube -->
+      <a-box 
+        position="0 1 -3" 
+        rotation="0 45 0" 
+        color="red" 
+        depth="0.5" 
+        height="0.5" 
+        width="0.5"
+        animation="property: position; to: 0 1.5 -3; dir: alternate; dur: 1500; loop: true"
+      ></a-box>
+
       <a-camera></a-camera>
     </a-scene>
   );
