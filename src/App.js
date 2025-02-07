f4007a832dbfbf18294bfcddae4117b091bebab6
elay.glazer <elay.glazer@controlup.com>
2025-02-07 16:12:49 +0200

    revert from floor

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ function App() {
       set1.setAttribute("visible", showSet1);
       set2.setAttribute("visible", !showSet1);
     }
-  }, [showSet1]); // Runs when showSet1 changes
+  }, [showSet1]);
 
   return (
     <>
-      {/* Left Arrow Button (Shows Set 2) */}
+      {/* Left Button (Show Set 2) */}
       <div 
         style={{
           position: "absolute",
           bottom: "20px",
-          left: "20px", // Aligns to bottom-left
+          left: "20px",
           zIndex: 100,
           width: "60px"
         }}
@@ -33,12 +33,12 @@ function App() {
         </button>
       </div>
 
-      {/* Right Arrow Button (Shows Set 1) */}
+      {/* Right Button (Show Set 1) */}
       <div 
         style={{
           position: "absolute",
           bottom: "20px",
-          right: "20px", // Aligns to bottom-right
+          right: "20px",
           zIndex: 100,
           width: "60px"
         }}
@@ -59,31 +59,16 @@ function App() {
             background-color: #000000;
             border: 2px solid #1A1A1A;
             border-radius: 15px;
-            box-sizing: border-box;
             color: #FFFFFF;
             cursor: pointer;
-            display: inline-block;
-            font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
             font-size: 30px;
             font-weight: 600;
-            line-height: normal;
-            margin: 0;
-            min-height: 60px;
-            min-width: 0;
-            outline: none;
             padding: 16px 24px;
             text-align: center;
-            text-decoration: none;
             transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
             user-select: none;
-            -webkit-user-select: none;
             touch-action: manipulation;
             width: 100%;
-            will-change: transform;
-          }
-
-          .button-27:disabled {
-            pointer-events: none;
           }
 
           .button-27:hover {
